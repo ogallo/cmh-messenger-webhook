e5588cdc512d409ff270c1ca2f63bc330b73cf58
Oscar Gallo Cannata <ogalloc@gmail.com>
2021-01-21 20:44:35 -0500

    Update index.js
    Obtener PSID.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 
 
-
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
@@ -23,10 +22,14 @@ app.post('/webhook', (req, res) => {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
     });
 
     // Returns a '200 OK' response to all requests
@@ -37,8 +40,6 @@ app.post('/webhook', (req, res) => {
   }
 
 });
-
-
 
 
 
@@ -69,3 +70,20 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
